a9617c6aea7e00053a4c8911a91b877ce40f4ba2
Denis Karpenko <denis@karpenko.me>
2013-01-27 15:42:21 +0300

    Update Antibody spec

--- app/models/antibody.rb
+++ app/models/antibody.rb
@@ -5,15 +5,10 @@ class Antibody
   validates_presence_of :name
 
   field :description, :type => String
-  #validates_presence_of :description
-
   field :result, :type => String
-
   field :reaction_mark, :type => String
   field :reaction_score_percentage, :type => Integer
   field :reaction_score_score, :type => Integer
-
-
 
   embedded_in :med_test, :inverse_of => :antibodies
 end
--- spec/models/antibody_spec.rb
+++ spec/models/antibody_spec.rb
@@ -4,7 +4,6 @@ describe Antibody do
 
   describe "Validation" do
     it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:description) }
   end
 
 end
