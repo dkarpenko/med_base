653ee010ebeb6dcc72cf8c7b7ce709906b35fd49
Denis Karpenko <denis@karpenko.me>
2013-01-30 01:51:58 +0300

    Update test configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,11 +16,22 @@ Spork.prefork do
   require 'rspec/autorun'
   require 'factory_girl'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
   require 'database_cleaner'
+
+  RSpec.configure do |config|
+    config.order = "random"
+
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.orm = "mongoid"
+
+    config.before(:each) do
+      DatabaseCleaner.clean
+    end
+
+  end
+
 
 end
 
@@ -33,37 +44,4 @@ Spork.each_run do
 end
 
 Spork.after_each_run do
-end
-
-
-RSpec.configure do |config|
-
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
-  # If true, the base class of anonymous controllers will be inferred
-  # automatically. This will be the default behavior in future versions of
-  # rspec-rails.
-  #config.infer_base_class_for_anonymous_controllers = false
-
-  # Run specs in random order to surface order dependencies. If you find an
-  # order dependency and want to debug it, you can fix the order by providing
-  # the seed, which is printed after each run.
-  #     --seed 1234
-  config.order = "random"
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.orm = "mongoid"
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.clean
-  end
-
 end
