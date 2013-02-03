d009f590068a527a6650445071cca43ac2f61c0e
Denis Karpenko <denis@karpenko.me>
2013-02-04 02:52:49 +0300

    Log in before test

--- spec/controllers/med_test_statistics_controller_spec.rb
+++ spec/controllers/med_test_statistics_controller_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe MedTestStatisticsController do
 
+  login_user
+
   describe 'GET index' do
     it 'assigns all statistics as @statistics' do
       test_statistic = FactoryGirl.create :med_test_statistic
