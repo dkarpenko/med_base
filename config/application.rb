204b5d63e1630d20998d3406b7af0acb9ad08d69
Denis Karpenko <denis@karpenko.me>
2013-02-06 10:44:51 +0300

    precompile med_test_statistics.js

--- config/application.rb
+++ config/application.rb
@@ -54,7 +54,7 @@ module Med
     config.assets.enabled = true
 
     config.assets.prefix = '/static'
-    config.assets.precompile += %w(application.js med_tests.js application.css bootstrap_fix.css bootstrap.js )
+    config.assets.precompile += %w(application.js med_tests.js med_test_statistics.js application.css bootstrap_fix.css bootstrap.js )
     config.assets.precompile += %w(jquery-tablesorter.js)
     #config.assets.precompile += ['application.js']
 
