2b8ff51ec73e4a6e6975b29f28490230b60c63e1
Denis Karpenko <denis@karpenko.me>
2014-01-13 00:01:42 +0300

    Remove spaces from antibodies names on save

--- app/models/med_test.rb
+++ app/models/med_test.rb
@@ -2,20 +2,27 @@ class MedTest
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :test_date, type: Date
 
   field :tracking_number, type: String
   validates_presence_of :tracking_number
 
+  field :test_date, type: Date
   field :doctor_client, type: String
   field :description, type: String
   field :conclusion, type: String
   field :patient_name, type: String
-
+  field :test_purpose, type: String
 
   attr_accessor :body_ids
 
-  embeds_many :antibodies
+  embeds_many :antibodies, before_add: :strip_name
 
-  field :test_purpose, type: String
+  private
+
+  def strip_name(antibody)
+    return false if antibody.name.blank?
+
+    antibody.name.strip! if antibody.name
+  end
+
 end
--- spec/models/med_test_spec.rb
+++ spec/models/med_test_spec.rb
@@ -2,8 +2,21 @@ require 'spec_helper'
 
 describe MedTest do
 
-  describe "Validation" do
+  describe 'Validation' do
     it { should validate_presence_of(:tracking_number) }
+  end
+
+  describe 'Cleaning antibodies' do
+    it 'should remove spaces from antibody name after adding it to collection' do
+      antibody = Antibody.new
+      antibody.name = '   name    '
+
+      med_test = MedTest.new
+      med_test.antibodies << antibody
+
+      med_test.antibodies[0].name.should eq('name')
+    end
+
   end
 
 end
